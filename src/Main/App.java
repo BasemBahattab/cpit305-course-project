9cf4c5e46b30ef28fbc87332b1610760563ed256
Basem <basem.bahattab.bm@gmail.com>
2023-03-22 18:29:17 +0300

    fix

--- src/Main/App.java
+++ src/Main/App.java
@@ -1,14 +1,13 @@
 package Main;
 
-
 import Connection.DataBase.DataBaseInit;
 import GUI.LoginGUI;
 
 public class App {
     public static void main(String[] args) {
         DataBaseInit DatabaseCreateTableIfNotExist = new DataBaseInit();
+        
         new LoginGUI().setVisible(true);
 
-        
     }
 }
